b4ace7fc71312d1439b754eac15d665adcc49081
Stewart Lynch <slynch@createchsol.com>
2020-11-26 15:32:47 -0800

    Initial Commit

---
+++ PieChart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PieChart
+//
+//  Created by Stewart Lynch on 2020-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PieChart/PieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartApp.swift
+//  PieChart
+//
+//  Created by Stewart Lynch on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct PieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
